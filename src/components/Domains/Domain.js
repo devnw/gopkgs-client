c2abf2fca10b708dc99f1820788ddd8d842e9e9b
Benji Vesterby <benji@devnw.com>
2022-04-20 06:14:23 -0400

    Working on domain list changes

--- src/components/Domains/Domain.js
+++ src/components/Domains/Domain.js
@@ -1,33 +1,60 @@
 import React, { Component } from "react";
-import Card from "@mui/material/Card";
+import { Card, Grid } from "@mui/material";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import {
+  faCircleExclamation,
+  faCircleCheck,
+} from "@fortawesome/free-solid-svg-icons";
 
 import "./Domain.scss";
 
 export default class Domain extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      id: props.id,
-      name: props.name,
-      description: props.description,
-      owner: props.owner,
-      maintainer: props.maintainer,
-      validateBy: props.validateBy,
-      validated: props.validated,
-      updated: props.updated,
-      token: props.token,
-      modules: props.modules,
-    };
+    this.id = props.id;
+    this.name = props.name;
+    this.description = props.description;
+    this.owner = props.owner;
+    this.maintainer = props.maintainer;
+    this.validateBy = props.validateBy;
+    this.validated = props.validated;
+    this.updated = props.updated;
+    this.token = props.token;
+    this.modules = props.modules;
   }
 
   render() {
-    console.log(this.state.id);
     return (
-      <Card key={this.state.id}>
-        <div>
-          <h2>{this.state.name}</h2>
-          <p>{this.state.description}</p>
-        </div>
+      <Card
+        sx={{
+          padding: "10px",
+          bgcolor: "#185A7D",
+          color: "#ffffff",
+          marginTop: "10px",
+        }}
+      >
+        <Grid container spacing={2} alignItems="center" justify="center">
+          <Grid item xs={10}>
+            <h2>{this.name}</h2>
+            <p>{this.description}</p>
+          </Grid>
+          <Grid item xs={2}>
+            {this.validated !== true ? (
+              <FontAwesomeIcon
+                size="4x"
+                icon={faCircleExclamation}
+                className="unverified"
+              />
+            ) : (
+              <FontAwesomeIcon
+                size="4x"
+                icon={faCircleCheck}
+                className="verified"
+              />
+            )}
+          </Grid>
+        </Grid>
       </Card>
     );
   }
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,43 +1,41 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import './NavBar.scss';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./NavBar.scss";
 
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-
-
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
 
 export default class NavBar extends Component {
   constructor(props) {
     super(props);
     this.pages = props.pages;
-    this.settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+    this.settings = ["Profile", "Account", "Dashboard", "Logout"];
 
     this.user = {
-      name: '',
-      avatar: '',
-    }
+      name: "",
+      avatar: "",
+    };
   }
 
   state = {
     anchorElNav: null,
     anchorElUser: null,
-  }
+  };
 
   handleOpenNavMenu = (event) => {
     this.setState({ anchorElNav: event.currentTarget });
   };
-  
+
   handleOpenUserMenu = (event) => {
     this.setState({ anchorElUser: event.currentTarget });
   };
@@ -50,7 +48,6 @@ export default class NavBar extends Component {
     this.setState({ anchorElUser: null });
   };
 
-
   render() {
     return (
       <AppBar position="static" sx={{ bgcolor: "#0E2E3F" }}>
@@ -60,107 +57,113 @@ export default class NavBar extends Component {
               variant="h6"
               noWrap
               component="div"
-              sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
+              sx={{ mr: 2, display: { xs: "none", md: "flex" } }}
             >
               <img
                 src={process.env.PUBLIC_URL + "/images/logos/logo.svg"}
                 className="logo"
-                alt="Developer Network Logo Symbol" />
+                alt="Developer Network Logo Symbol"
+              />
             </Typography>
 
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={this.handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={this.state.anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(this.state.anchorElNav)}
-              onClose={this.handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
-              {this.pages.map((page) => (
-                <MenuItem
-                  key={page}
-                  onClick={this.handleCloseNavMenu}
-                  component={Link}
-                  to={'/' + page.toLowerCase()}>
-                  <Typography textAlign="center" sx={{ textTransform: "capitalize" }}>
-                    {page}
-                  </Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
+            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={this.handleOpenNavMenu}
+                color="inherit"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={this.state.anchorElNav}
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "left",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "left",
+                }}
+                open={Boolean(this.state.anchorElNav)}
+                onClose={this.handleCloseNavMenu}
+                sx={{
+                  display: { xs: "block", md: "none" },
+                }}
+              >
+                {this.pages.map((page) => (
+                  <MenuItem
+                    key={page}
+                    onClick={this.handleCloseNavMenu}
+                    component={Link}
+                    to={"/" + page.toLowerCase()}
+                  >
+                    <Typography
+                      textAlign="center"
+                      sx={{ textTransform: "capitalize" }}
+                    >
+                      {page}
+                    </Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
 
-            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
               {this.pages.map((page) => (
                 <Button
                   component={Link}
-                  to={'/' + page.toLowerCase()}
+                  to={"/" + page.toLowerCase()}
                   key={page}
                   // className="app-links"
                   onClick={this.handleCloseNavMenu}
-                  sx={{ 
+                  sx={{
                     my: 2,
-                    color: 'white',
-                    display: 'block',
-                  '&:hover': {
-                    background: "#185A7D",
-                  },}}
+                    color: "white",
+                    display: "block",
+                    "&:hover": {
+                      background: "#185A7D",
+                    },
+                  }}
                 >
-                    {page}
+                  {page}
                 </Button>
               ))}
             </Box>
 
-           <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={this.handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt={this.user.name} src={this.user.avatar} />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={this.state.anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(this.state.anchorElUser)}
-              onClose={this.handleCloseUserMenu}
-            >
-              {this.settings.map((setting) => (
-                <MenuItem key={setting} onClick={this.handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
+            <Box sx={{ flexGrow: 0 }}>
+              <Tooltip title="Open settings">
+                <IconButton onClick={this.handleOpenUserMenu} sx={{ p: 0 }}>
+                  <Avatar alt={this.user.name} src={this.user.avatar} />
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ mt: "45px" }}
+                id="menu-appbar"
+                anchorEl={this.state.anchorElUser}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={Boolean(this.state.anchorElUser)}
+                onClose={this.handleCloseUserMenu}
+              >
+                {this.settings.map((setting) => (
+                  <MenuItem key={setting} onClick={this.handleCloseUserMenu}>
+                    <Typography textAlign="center">{setting}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
           </Toolbar>
         </Container>
       </AppBar>
