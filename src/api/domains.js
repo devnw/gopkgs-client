c5a51a155b5386bdc4602723ab223dfe6ee4f1f0
Benji Vesterby <benji@devnw.com>
2022-05-09 19:46:11 -0400

    Added support for error alerting

--- src/api/domains.js
+++ src/api/domains.js
@@ -26,49 +26,23 @@ const getDomains = async (getAccessTokenSilently, setDomains) => {
 };
 
 const putDomain = async (getAccessTokenSilently, domain) => {
-  try {
-    const accessToken = await getAccessTokenSilently({
-      audience: `${process.env.REACT_APP_AUTH0_AUDIENCE}`,
-      scope: "read:current_user",
-    });
+  const accessToken = await getAccessTokenSilently({
+    audience: `${process.env.REACT_APP_AUTH0_AUDIENCE}`,
+    scope: "read:current_user",
+  });
 
-    console.log(domain);
-    console.log(JSON.stringify({ domain: domain }));
+  const response = await fetch(
+    `${process.env.REACT_APP_API_SERVER_URL}/domains`,
+    {
+      method: "PUT",
+      body: JSON.stringify({ domain: domain }),
+      headers: new Headers({
+        Authorization: `Bearer ${accessToken}`,
+      }),
+    }
+  );
 
-    const response = await fetch(
-      `${process.env.REACT_APP_API_SERVER_URL}/domains`,
-      {
-        method: "PUT",
-        body: JSON.stringify({ domain: domain }),
-        headers: new Headers({
-          Authorization: `Bearer ${accessToken}`,
-        }),
-      }
-    );
-
-    return await response.json();
-  } catch (error) {
-    console.log(error);
-    // setState({
-    //   ...state,
-    //   error: error.error,
-    // });
-  }
+  return await response.json();
 };
 
 export { getDomains, putDomain };
-
-// const getDomains = async (auth) => {
-//   const response = await fetch("https://api.gopkgs.org:2096/domains", {
-//     method: "GET",
-//     body: myBody, // string or object
-//     headers: {
-//       "Content-Type": "application/json",
-//       Authorization: `Bearer ${auth}`,
-//     },
-//   });
-
-//   return await response.json();
-// };
-
-// export default getDomains;
