efea5ce40ca7d8bd4c1a6e7bfd06f5cd897a1d33
Benji Vesterby <benji@devnw.com>
2022-05-23 20:14:59 -0400

    Working on the domain page styling

--- src/components/Domains/Domain.js
+++ src/components/Domains/Domain.js
@@ -1,7 +1,19 @@
 import React from "react";
-
-import { Card, Grid, Tooltip, IconButton } from "@mui/material";
+import copy from "clipboard-copy";
+import {
+  Card,
+  CardActionArea,
+  CardActions,
+  Grid,
+  Tooltip,
+  IconButton,
+} from "@mui/material";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import ReplayIcon from "@mui/icons-material/Replay";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 
 import {
   faCircleExclamation,
@@ -26,48 +38,101 @@ const Domain = (props) => {
     setValidateOpen(false);
   };
 
+  const requestToken = () => {};
+
+  const reVerify = () => {};
+
+  const handleDelete = () => {};
+
+  const onCopy = (content) => {
+    copy(props.data);
+    props.alert({
+      open: true,
+      message: "TXT Record copied to clipboard!",
+      severity: "success",
+    });
+  };
+
   return (
     <Card
+      className="domain-summary"
       sx={{
-        padding: "10px",
-        bgcolor: "#185A7D",
-        color: "#ffffff",
         marginTop: "10px",
+        borderRadius: "0px",
       }}
     >
-      <Grid container spacing={2} alignItems="center" justify="center">
-        <Grid item xs={8} md={10}>
-          <h2>{props.domain}</h2>
-          <p>{props.description}</p>
-          {props.modules?.length > 0 ? (
-            <h3>Registered Modules: {props.modules.length}</h3>
-          ) : null}
+      <CardActionArea onClick={handleValidateOpen}>
+        <Grid
+          container
+          spacing={2}
+          alignItems="center"
+          justify="center"
+          className="domain-name-background"
+        >
+          <Grid item xs={8} md={10}>
+            <h2>{props.domain}</h2>
+            <p>{props.description}</p>
+            {props.modules?.length > 0 ? (
+              <h3>Registered Modules: {props.modules.length}</h3>
+            ) : null}
+          </Grid>
+          <Grid item xs={4} md={2} sx={{ textAlign: "right" }}>
+            {!props.validated ? (
+              <Tooltip title={"This domain is not validated yet"}>
+                <IconButton onClick={handleValidateOpen}>
+                  <FontAwesomeIcon
+                    size="4x"
+                    icon={faCircleExclamation}
+                    className="unverified"
+                  />
+                </IconButton>
+              </Tooltip>
+            ) : (
+              <Tooltip title={"This domain has been verified"}>
+                <IconButton onClick={handleValidateOpen}>
+                  <FontAwesomeIcon
+                    size="4x"
+                    icon={faCircleCheck}
+                    className="verified"
+                  />
+                </IconButton>
+              </Tooltip>
+            )}
+          </Grid>
         </Grid>
-        <Grid item xs={4} md={2}>
-          {!props.validated ? (
-            <Tooltip title={"This domain is not validated yet"}>
-              <IconButton onClick={handleValidateOpen}>
-                <FontAwesomeIcon
-                  size="4x"
-                  icon={faCircleExclamation}
-                  className="unverified"
-                />
-              </IconButton>
-            </Tooltip>
-          ) : (
-            <Tooltip title={"This domain has been verified"}>
-              <IconButton onClick={handleValidateOpen}>
-                <FontAwesomeIcon
-                  size="4x"
-                  icon={faCircleCheck}
-                  className="verified"
-                />
-              </IconButton>
-            </Tooltip>
-          )}
-        </Grid>
-      </Grid>
+      </CardActionArea>
+      <CardActions>
+        <Grid container spacing={2}>
+          <Grid fullWidth item xs={12} sx={{ textAlign: "right" }}>
+            {!props.validated ? (
+              <>
+                <IconButton>
+                  <Tooltip title={"Copy TXT Record"}>
+                    <ContentCopyIcon fontSize="large" onClick={onCopy} />
+                  </Tooltip>
+                </IconButton>
 
+                <IconButton>
+                  <Tooltip title={"New Token"}>
+                    <ReplayIcon fontSize="large" onClick={requestToken} />
+                  </Tooltip>
+                </IconButton>
+                <IconButton>
+                  <Tooltip title={"Verify Domain"}>
+                    <CheckCircleIcon fontSize="large" onClick={reVerify} />
+                  </Tooltip>
+                </IconButton>
+              </>
+            ) : null}
+
+            <IconButton>
+              <Tooltip title={"Delete Domain"}>
+                <DeleteForeverIcon fontSize="large" onClick={handleDelete} />
+              </Tooltip>
+            </IconButton>
+          </Grid>
+        </Grid>
+      </CardActions>
       <ValidateDomain
         alert={props.alert}
         id={props.id}
