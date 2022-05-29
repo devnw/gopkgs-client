45f202dc671381436a69299f36ddfcca09e15418
Benji Vesterby <benji@devnw.com>
2022-05-29 06:36:31 -0400

    Added the ability to copy a header link

--- src/components/Ref.js
+++ src/components/Ref.js
@@ -1,16 +1,51 @@
-import React from "react";
-import { Typography } from "@mui/material";
+import copy from "clipboard-copy";
+import React, { useState } from "react";
+import { Typography, Grid, Tooltip, IconButton } from "@mui/material";
+import ShareOutlinedIcon from "@mui/icons-material/ShareOutlined";
+import { useLocation } from "react-router-dom";
 
 const Ref = (props) => {
+  const [showTooltip, setShowTooltip] = useState(false);
+  const id = props.children.replace(/ /g, "-").toLowerCase();
+  const location = useLocation();
+  const url = new URL(
+    `${process.env.REACT_APP_URL}${location.pathname}#${encodeURIComponent(id)}`
+  ).toString();
+
+  const onCopy = (content) => {
+    copy(url);
+    setShowTooltip(true);
+  };
+
+  const handleOnTooltipClose = () => {
+    setShowTooltip(false);
+  };
+
   if (!props.variant.startsWith("h")) {
     return <Typography {...props} />;
   }
 
   return (
-    <Typography
-      {...props}
-      id={props.children.replace(/ /g, "-").toLowerCase()}
-    />
+    <Grid container>
+      <Grid item>
+        <Typography {...props} id={id} />
+      </Grid>
+      <Grid item>
+        <Tooltip
+          open={showTooltip}
+          title={"Copied to clipboard!"}
+          leaveDelay={300}
+          onClose={handleOnTooltipClose}
+          {...(props.TooltipProps || {})}
+          onClick={onCopy}
+          data={props.data}
+        >
+          <IconButton onClick={onCopy}>
+            <ShareOutlinedIcon fontSize="small" />
+          </IconButton>
+        </Tooltip>
+      </Grid>
+    </Grid>
   );
 };
 
